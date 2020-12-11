5e0ce0143c7a5e691af5ccd4ea419d4134b653c9
Valeria Avila <valeria.avila17@gmail.com>
2020-12-11 01:59:27 -0800

    Edited profile page. Added productViewModel and  controller action to add file to product post

--- ShowcaseSite/Controllers/HomeController.cs
+++ ShowcaseSite/Controllers/HomeController.cs
@@ -1,31 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ShowcaseSite.Data;
 using ShowcaseSite.Models;
+using ShowcaseSite.ViewModels;
 
 namespace ShowcaseSite.Controllers
 {
     public class HomeController : Controller
     {
         private UserManager<ApplicationUser> _userManager;
-
-
+        private readonly ApplicationDbContext dbContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
+        public char[] UploadedFiles { get; private set; }
+
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
         {
             _userManager = userManager;
             _logger = logger;
+            dbContext = context;
+            webHostEnvironment = hostEnvironment;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var product = await dbContext.Products.ToListAsync();
+            return View();
+        }
+
+        public IActionResult Profile()
         {
             return View();
+        }
+
+        
+        
+        private string UploadedFile(ProductViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if(model.PicUrl != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.PicUrl.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.PicUrl.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
         }
 
         public IActionResult Privacy()
@@ -33,13 +67,33 @@ namespace ShowcaseSite.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Profile()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Profile(ProductViewModel model)
         {
             ApplicationUser currentUser = await _userManager.GetUserAsync(User);
          
             ViewBag.Message = currentUser.Bio;
             ViewBag.Name = currentUser.FirstName + " " + currentUser.LastName;
-            
+
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = UploadedFile(model);
+
+                Product product = new Product
+                {
+                    UserName = currentUser.UserName,
+                    Title = model.Title,
+                    Description = model.Description,
+                    Price = model.Price,
+                    PicUrl = uniqueFileName
+                };
+
+                dbContext.Add(product);
+                await dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
             return View();
         }
 
---
+++ ShowcaseSite/ViewModels/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowcaseSite.ViewModels
+{
+    public class ProductViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public IFormFile PicUrl { get; set; }
+    }
+}
